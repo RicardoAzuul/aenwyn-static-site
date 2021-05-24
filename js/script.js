1132c2b158302bbcf30add9662c731cecc9cf76b
Richard Blaauw <richard.blaauw@ogd.nl>
2021-05-24 19:30:48 +0200

    Change JavaScript code to store lvl 0 spell slots as name/value pairs.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
+/* 
+TODO: 
+- get an HTML element from the page based on class --> by class means we can target any spell lvl counter
+- get the ID, then use that for as the name for the name value pair in localStorage
+- if the name/value pair doesn't exist, set it equal to the total spell slots
+- if it does exist, read the value and put it into the HTML 
+
+*/
+
 function addSlot() {
     if (typeof (Storage) !== "undefined") {
-        if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        if (localStorage.getItem('lvl-0-spellslots')) {
+            let tempNumber = parseInt(localStorage.getItem('lvl-0-spellslots'));
+            tempNumber++;
+            localStorage.setItem('lvl-0-spellslots', tempNumber);
         } else {
-            localStorage.clickcount = 1;
+            localStorage.setItem('lvl-0-spellslots', 6);
         }
-        document.getElementById("result").innerHTML = localStorage.clickcount;
+        document.getElementById("slots-left").innerHTML = localStorage.getItem('lvl-0-spellslots');
     } else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        document.getElementById("slots-left").innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
 function subtractSlot() {
     if (typeof (Storage) !== "undefined") {
-        if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount) - 1;
+        if (localStorage.getItem('lvl-0-spellslots')) {
+            let tempNumber = parseInt(localStorage.getItem('lvl-0-spellslots'));
+            tempNumber--;
+            localStorage.setItem('lvl-0-spellslots', tempNumber);
         } else {
-            localStorage.clickcount = 1;
+            localStorage.setItem('lvl-0-spellslots', 6);
         }
-        document.getElementById("result").innerHTML = localStorage.clickcount;
+        document.getElementById("slots-left").innerHTML = localStorage.getItem('lvl-0-spellslots');
     } else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        document.getElementById("slots-left").innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
